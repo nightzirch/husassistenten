d4f11b53fb84a26c0f7a6984bc8fc54803a078ac
Christian Grimsgaard <christian.grimsgaard@knowit.no>
2016-02-12 17:31:06 +0100

    Initial commit with KeystoneJS

---
+++ keystone.js
@@ -0,0 +1,44 @@
+// Simulate config options from your production environment by
+// customising the .env file in your project's root folder.
+require('dotenv').load();
+
+// Require keystone
+var keystone = require('keystone');
+
+keystone.init({
+
+	'name': 'Husassistenten',
+	'brand': 'Husassistenten',
+	
+	'sass': 'public',
+	'static': 'public',
+	'favicon': 'public/favicon.ico',
+	'views': 'templates/views',
+	'view engine': 'jade',
+	
+	'auto update': true,
+	'session': true,
+	'auth': true,
+	'user model': 'User'
+
+});
+
+keystone.import('models');
+
+keystone.set('locals', {
+	_: require('underscore'),
+	env: keystone.get('env'),
+	utils: keystone.utils,
+	editable: keystone.content.editable
+});
+
+keystone.set('routes', require('./routes'));
+
+keystone.set('nav', {
+	'posts': ['posts', 'post-categories'],
+	'galleries': 'galleries',
+	'enquiries': 'enquiries',
+	'users': 'users'
+});
+
+keystone.start();
