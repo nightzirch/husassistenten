1ccf62e389e320d1b1f65441c24cb7c39e5ac3ec
Christian Grimsgaard <christian.grimsgaard@knowit.no>
2016-02-15 17:10:03 +0100

    Disabled some views

--- routes/middleware.js
+++ routes/middleware.js
@@ -6,20 +6,17 @@ var _ = require('underscore');
 */
 
 exports.initLocals = function(req, res, next) {
-	
+
 	var locals = res.locals;
-	
+
 	locals.navLinks = [
-		{ label: 'Home',		key: 'home',		href: '/' },
-		{ label: 'Blog',		key: 'blog',		href: '/blog' },
-		{ label: 'Gallery',		key: 'gallery',		href: '/gallery' },
-		{ label: 'Contact',		key: 'contact',		href: '/contact' }
+		{ label: 'Home',		key: 'home',		href: '/' }
 	];
-	
+
 	locals.user = req.user;
-	
+
 	next();
-	
+
 };
 
 
@@ -28,18 +25,18 @@ exports.initLocals = function(req, res, next) {
 */
 
 exports.flashMessages = function(req, res, next) {
-	
+
 	var flashMessages = {
 		info: req.flash('info'),
 		success: req.flash('success'),
 		warning: req.flash('warning'),
 		error: req.flash('error')
 	};
-	
+
 	res.locals.messages = _.any(flashMessages, function(msgs) { return msgs.length; }) ? flashMessages : false;
-	
+
 	next();
-	
+
 };
 
 
@@ -48,12 +45,12 @@ exports.flashMessages = function(req, res, next) {
  */
 
 exports.requireUser = function(req, res, next) {
-	
+
 	if (!req.user) {
 		req.flash('error', 'Please sign in to access this page.');
 		res.redirect('/keystone/signin');
 	} else {
 		next();
 	}
-	
+
 };
